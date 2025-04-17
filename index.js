ac0ed7cb6432bc18ee9952e74fedc70b9ba41076
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2025-04-17 17:01:09 +1000

    updated item function has been updated

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
@@ -24,12 +35,57 @@ const getAllTable = (req, res) => {
     res.status(200).json(results.rows);
   });
 };
+const getItemByID = (req, res) => {
+  const { cart } = req.body;
+  console.log(cart);
+
+  pool.query(
+    "select * from production where id =  ANY($1::int[])",
+    [cart],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+};
+const updateCart = (req, res) => {
+  console.log(req);
+  const { cart } = req.body;
+  console.log(cart);
+  const id = Math.floor(Math.random() * 100);
+  pool.query(
+    "insert into cart(id,production_id) values ($1, $2)",
+    [id, cart],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+};
+const getAllCart = (req, res) => {
+  pool.query("SELECT * FROM cart ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/cart", getAllCart);
+app.post("/cart", updateCart);
+//
+app.post("/cardID", getItemByID);
+//
 app.get("/production", getAllTable);
-const port = 3000;
+//
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
 });
